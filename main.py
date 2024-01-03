90a7b25893ea974c1867dc6830597ca3a91a6c6f
ionut <ionutpad15@gmail.com>
2024-01-03 14:45:01 +0200

    added the baseline and fasttext files

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import generate_graph as gg
 import process_text
+import fasttext_method as ft
+
 
 if __name__ == "__main__":
     #result = gg.get_graph()
     process_text.process("train")
     process_text.process("dev_unlabeled")
     process_text.process("validation")
+    ft.ft_train()
